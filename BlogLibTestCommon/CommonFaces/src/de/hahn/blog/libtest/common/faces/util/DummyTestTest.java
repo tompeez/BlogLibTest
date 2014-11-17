7cdce5cb9883dd333b8157a21a49205a21194bc2
Timo Hahn <hahn@virtual7.de>
2014-11-17 17:17:22 +0100

    sonar for projects implemented

---
+++ BlogLibTestCommon/CommonFaces/src/de/hahn/blog/libtest/common/faces/util/DummyTestTest.java
@@ -0,0 +1,38 @@
+package de.hahn.blog.libtest.common.faces.util;
+
+import org.junit.Test;
+
+public class DummyTestTest {
+    public DummyTestTest() {
+    }
+
+    /**
+     * @see DummyTest#test1()
+     */
+    @Test
+    public void testTest1() {
+        DummyTest dt = new DummyTest();
+        dt.test1();
+        assert (true);
+    }
+
+    /**
+     * @see DummyTest#test2()
+     */
+    @Test
+    public void testTest2() {
+        DummyTest dt = new DummyTest();
+        dt.test2();
+        assert (true);
+    }
+
+    /**
+     * @see DummyTest#test3()
+     */
+    @Test
+    public void testTest3() {
+        DummyTest dt = new DummyTest();
+        dt.test3();
+        assert (true);
+    }
+}
